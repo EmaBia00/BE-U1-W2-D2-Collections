d4e19e6b0f0efbeb306370fd3dae5b0d93958d8b
Emanuele Bianchi <emanuelebianchi00@gmail.com>
2025-01-14 16:42:37 +0100

    End Es02

--- src/Esercizio2.java
+++ src/Esercizio2.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Esercizio2 {
+    public List<Integer> generaNumeriCasuali(int n) {
+        List<Integer> numeri = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++) {
+            numeri.add(random.nextInt(101));
+        }
+
+        Collections.sort(numeri);
+        return numeri;
+    }
+
+    public List<Integer> invertiEUnisci(List<Integer> lista) {
+        List<Integer> invertita = new ArrayList<>(lista);
+        Collections.reverse(invertita);
+        List<Integer> unita = new ArrayList<>(lista);
+        unita.addAll(invertita);
+        return unita;
+    }
+
+    public void stampaElementi(List<Integer> lista, boolean pari) {
+        System.out.println(pari ? "Elementi in posizioni pari:" : "Elementi in posizioni dispari:");
+        for (int i = 0; i < lista.size(); i++) {
+            if ((i % 2 == 0 && pari) || (i % 2 != 0 && !pari)) {
+                System.out.print(lista.get(i) + " ");
+            }
+        }
+        System.out.println();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Esercizio 1:");
         Esercizio1 esercizio1 = new Esercizio1();
         esercizio1.esegui();
+
+        System.out.println("\nEsercizio 2:");
+        Esercizio2 esercizio2 = new Esercizio2();
+
+        int n = 10;
+        List<Integer> numeriCasuali = esercizio2.generaNumeriCasuali(n);
+        System.out.println("Lista ordinata di numeri casuali: " + numeriCasuali);
+
+        List<Integer> listaUnita = esercizio2.invertiEUnisci(numeriCasuali);
+        System.out.println("Lista unita con l'inversione: " + listaUnita);
+
+        esercizio2.stampaElementi(listaUnita, true);
+        esercizio2.stampaElementi(listaUnita, false);
     }
 }
